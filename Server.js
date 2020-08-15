32b58addd305f4f88499a3dda878e85d12ae25a5
santhosh21051998 <santhosh21051998@gmail.com>
2020-08-15 21:12:42 +0530

    commenting page check

--- Server.js
+++ Server.js
@@ -24,10 +24,10 @@ const
       // Returns a '200 OK' response to all requests
       res.status(200).send('EVENT_RECEIVED');
     //} 
-     else {
+     /*else {
       // Returns a '404 Not Found' if event is not from a page subscription
       res.sendStatus(404);
-    }
+    } */
   
   });
 
